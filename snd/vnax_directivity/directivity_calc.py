aec87d06423b6dca5c8b75a46faf35d028398d84
steven durant <durant@vadiodes.com>
2026-04-09 09:43:46 -0400

    add residual directivity plot

--- snd/vnax_directivity/directivity_calc.py
+++ snd/vnax_directivity/directivity_calc.py
@@ -33,26 +33,53 @@ def plot_reflection_trackings(df):
 
 	plt.show()
 
+def plot_residual_directivity(xlsx='vnax4037b.xlsx'):
+
+	df = pd.read_excel(xlsx,sheet_name='xj10',index_col=0)
+	df['S11(lin)'] = np.power(10,df['S11(dB)']/20)
+
+	s11vals = df['S11(lin)'].values
+	derivative = list( s11vals[1:]-s11vals[:-1] )
+	derivative.append(0)
+
+	crossings_indices = np.where(np.diff(np.sign(derivative)))[0]
+
+	df['deriv'] = derivative
+	df['crossings'] = 0
+	for j in crossings_indices:
+		df['crossings'].iloc[j] = 1
+
+
+	subdf = df[df['crossings']>0]
+	peakvals = subdf['S11(lin)'].values
+	cross_indices = subdf.index.values
+
+	peakdiffs = peakvals[:-1]-peakvals[1:]
+	peakdiffs_db = 20*np.log10( abs(peakdiffs) )
+
+	peak_indices = (cross_indices[:-1]+cross_indices[1:])/2
+
+
+	return df, peakdiffs_db, peak_indices
 
 
 
+#if __name__ == '__main__':
+	# plt.ion()
 
-if __name__ == '__main__':
-	plt.ion()
+	# #df = pd.read_excel('TRL_coefs.xlsx')
+	# df = pd.read_excel('SOLT_coefs.xlsx')
+	# df.index = df['Unnamed: 0']
 
-	#df = pd.read_excel('TRL_coefs.xlsx')
-	df = pd.read_excel('SOLT_coefs.xlsx')
-	df.index = df['Unnamed: 0']
+	# dir1 = plot_directivity(df, 'forward')
+	# dir2 = plot_directivity(df, 'reverse')
 
-	dir1 = plot_directivity(df, 'forward')
-	dir2 = plot_directivity(df, 'reverse')
+	# plot_reflection_trackings(df)
+	# plt.figure()
 
-	plot_reflection_trackings(df)
-	plt.figure()
+	# (-1*dir1).plot()
+	# (-1*dir2).plot()
 
-	(-1*dir1).plot()
-	(-1*dir2).plot()
+	# plt.axhline(-18)
 
-	plt.axhline(-18)
-
-	plt.show()
+	# plt.show()
